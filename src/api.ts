d04b72cb6bc24beb3ff2ba85cdbf1975fd0bb5dc
stLmpp <gui.stlmpp@gmail.com>
2023-09-14 19:44:38 -0300

    added create api

--- src/api.ts
+++ src/api.ts
@@ -1,30 +1,4 @@
-import { defineSecret } from 'firebase-functions/params';
-import { https } from 'firebase-functions/v2';
-
 import { AppModule } from './app.module';
-import { createNestApp } from './core/create-nest-app';
+import { createApi } from './core/create-api';
 
-const DATABASE_URL = defineSecret('DATABASE_URL');
-
-export const api = https.onRequest(
-  {
-    secrets: [DATABASE_URL],
-    // TODO figure out a way to configure this in the build process (github action)
-    timeoutSeconds: 20,
-    cpu: 1,
-    region: 'us-east1',
-    minInstances: 0,
-    maxInstances: 2,
-    concurrency: 50,
-    memory: '256MiB',
-  },
-  async (request, response) => {
-    const [app] = await createNestApp({
-      secrets: {
-        DATABASE_URL,
-      },
-      module: AppModule,
-    });
-    app(request, response);
-  },
-);
+export const api = createApi(AppModule);
